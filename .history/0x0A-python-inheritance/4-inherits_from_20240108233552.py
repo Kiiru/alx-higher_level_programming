231be67ac5d467e37789dda870eff78f2745c138
Kiiru <mwangi.ramson@gmail.com>
2024-01-08 23:38:11 +0300

    4-inherits_from.py

---
+++ .history/0x0A-python-inheritance/4-inherits_from_20240108233552.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""function that returns True if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class ; otherwise False."""
+def inherits_from(obj, a_class):
+    """function: is_same_class 
+    Args:
+        obj: object
+        a_class: class to check obj against
+    Returns:
+        True if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class ; otherwise False.
+    """
+    return isinstance(obj, a_class)
