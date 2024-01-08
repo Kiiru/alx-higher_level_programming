fa08411d780813ceba9a7d200dd09a4b3eac1a4b
Kiiru <mwangi.ramson@gmail.com>
2024-01-08 23:09:43 +0300

    2-is_same_class.py

---
+++ .history/0x0A-python-inheritance/2-is_same_class_20240108230635.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+"""function  returns True if the object is exactly an instance of the specified class ; otherwise False."""
---
+++ .history/0x0A-python-inheritance/2-is_same_class_20240108230732.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""function that returns True if the object is exactly an instance of the specified class ; otherwise False."""
+def is_same_class(obj, a_class):
+    """function: is_same_class 
+    Args:
+        obj: object
+        a_class
+    Returns:
+        a list object
+    """
