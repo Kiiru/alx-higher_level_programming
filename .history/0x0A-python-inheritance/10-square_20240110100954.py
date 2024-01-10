4eaed8519272436ec819111b721cf34d6e4a9a3c
Kiiru <mwangi.ramson@gmail.com>
2024-01-10 10:10:05 +0300

    10-square.py

--- .history/0x0A-python-inheritance/10-square_20240110100954.py
+++ .history/0x0A-python-inheritance/10-square_20240110100954.py
@@ -9,7 +9,6 @@ class Square(Rectangle):
     """ definition of a Square """
     def __init__(self, size):
         """ constructor and size """
-        Rectangle.integer_validator(self, 'size', self.__size)
         self.__size = size
         super().__init__(self.__size, self.__size)
 
