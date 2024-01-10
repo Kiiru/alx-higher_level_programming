f34c71870ce732ac822b5415737e416e903f9c4e
Kiiru <mwangi.ramson@gmail.com>
2024-01-10 10:23:18 +0300

    11-square.py

---
+++ .history/0x0A-python-inheritance/11-square_20240110102037.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+more class base
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+"""
+Square class
+"""
+class Square(Rectangle):
+    """ definition of a Square """
+    def __init__(self, size):
+        """ constructor and size """
+        self.__size = size
+        Rectangle.integer_validator(self, 'size', self.__size)
+        super().__init__(self.__size, self.__size)
+        
+    def area(self):
+        return self.__size * self.__size
+    
+    def __str__(self):
+        """ print """
+        return ("[Rectangle] " + str(self.__width) + "/" + str(self.__height))
