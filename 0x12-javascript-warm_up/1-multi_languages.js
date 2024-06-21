36c4d6b74d76d5481ec96927aa854ea1c9908744
Kiiru <mwangi.ramson@gmail.com>
2024-06-21 14:20:10 +0300

    1-multi_languages.js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const myVarC = 'C is fun';
+const myVarP = 'Python is cool';
+const myVarJ = 'Javascript is amazing';
+
+console.log(myVarC);
+console.log(myVarP);
+console.log(myVarJ);
+
